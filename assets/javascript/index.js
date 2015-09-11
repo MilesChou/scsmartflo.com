61b940931836ca24a3d5a8cddcbafa17f3d45f9e
eric <eric7578@gmail.com>
2015-09-11 09:02:38 +0800

    complete contact, index

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,15 +2,38 @@
  * Created by eric7578 on 15/8/30.
  */
 $(function () {
-    var $navList = $('.navbar-nav').find('li');
-    var $productNav = $('.product-nav');
+    var $navList = $('.navbar-list').find('li');
 
     $navList.click(function () {
         $navList.removeClass('current');
         $(this).addClass('current');
     });
 
-    $('img').click(function () {
-        $('#product-modal').modal();
+    var $products = $('.products');
+    var $contactUs = $('.contact-us');
+    var $introContent = $('.intro-content');
+    $(window).on('resize', function () {
+        $products.css('height', $(this).height());
+        $contactUs.css('height', $(this).height());
+        console.log($(this).width())
+        $introContent.css('height', $(this).width() * 400 / 1024);
     });
+
+    $('.intro-carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplaySpeed: 5000,
+        prevArrow: $('.prev-btn'),
+        nextArrow: $('.next-btn')
+    });
+
+    var $productNow = $('.product-now').detach();
+
+    $('.product-item').click(function () {
+        $(this).after($productNow);
+        $productNow.fadeIn('slow', function () {
+            alert('123')
+        });
+    })
+
 });
