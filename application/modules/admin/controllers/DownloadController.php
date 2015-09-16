7b1e89b9b4c7bafd3f5db96deb2b7fad5bce484d
MilesChou <jangconan@gmail.com>
2015-09-17 00:19:47 +0800

    Added download service

--- application/modules/admin/controllers/DownloadController.php
+++ application/modules/admin/controllers/DownloadController.php
@@ -11,5 +11,37 @@ class Admin_DownloadController extends Zend_Controller_Action
     public function indexAction()
     {
         $downloadService = new Application_Service_Download();
+
+        $this->view->downloads = $downloadService->getDownloads();
+    }
+
+    public function addAction()
+    {
+        $this->getHelper('viewRenderer')->setNoRender();
+        $this->getHelper('layout')->disableLayout();
+
+        $request = $this->getRequest();
+        $file = new Zend_File_Transfer_Adapter_Http();
+
+        $downloadService = new Application_Service_Download();
+        $downloadService->addDownload($request, $file);
+    }
+
+    public function updAction()
+    {
+        $this->getHelper('viewRenderer')->setNoRender();
+        $this->getHelper('layout')->disableLayout();
+
+        $downloadService = new Application_Service_Download();
+
+    }
+
+    public function delAction()
+    {
+        $this->getHelper('viewRenderer')->setNoRender();
+        $this->getHelper('layout')->disableLayout();
+
+        $downloadService = new Application_Service_Download();
+
     }
 }
--- application/services/Download.php
+++ application/services/Download.php
@@ -2,4 +2,66 @@
 
 class Application_Service_Download
 {
+    /**
+     * @var Application_Model_Download
+     */
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = new Application_Model_Download();
+    }
+
+    /**
+     * @param Zend_Controller_Request_Http
+     * @param Zend_File_Transfer_Adapter_Http $file
+     */
+    public function addDownload($request, $file)
+    {
+        $post = $request->getPost();
+        $fileInfo = $file->getFileInfo();
+
+        if (isset($fileInfo['file']) && $fileInfo['file']['error'] == 0) {
+            $filename = time() . $fileInfo['file']['name'];
+            $file->addFilter('Rename', array('target' => APPLICATION_PATH . '/../public/upload/' . $filename, 'overwrite' => true));
+
+            $file->receive();
+
+            $this->model->addDownload(
+                $post['title'],
+                $filename
+            );
+        }
+    }
+
+    /**
+     * @param Zend_Controller_Request_Http
+     * @param Zend_File_Transfer_Adapter_Http $file
+     */
+    public function updProduct($request, $file)
+    {
+        $post = $request->getPost();
+        $fileInfo = $file->getFileInfo();
+
+        if (isset($fileInfo['file']) && $fileInfo['file']['error'] == 0) {
+            $filename = time() . $fileInfo['file']['name'];
+            $file->addFilter('Rename', array('target' => APPLICATION_PATH . '/../public/upload/' . $filename, 'overwrite' => true));
+
+            $file->receive();
+
+            $this->model->updDownload(
+                $post['title'],
+                $filename
+            );
+        }
+    }
+
+    /**
+     * @return Zend_Db_Table_Rowset_Abstract
+     */
+    public function getDownloads()
+    {
+        return $this->model->getDownload();
+    }
+
 }
