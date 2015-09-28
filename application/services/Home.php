bbc90f63dae01facd42719cb8f1a103340d0ea54
MilesChou <jangconan@gmail.com>
2015-09-28 18:50:53 +0800

    Upgraded home service

--- application/services/Home.php
+++ application/services/Home.php
@@ -2,7 +2,57 @@
 
 class Application_Service_Home
 {
-    public function getInfo()
+    /**
+     * @var Application_Model_Home
+     */
+    private $model;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
     {
+        $this->model = new Application_Model_Home();
+    }
+
+    /**
+     * @param Zend_File_Transfer_Adapter_Http $file
+     * @param string $description
+     */
+    public function addSliderShow($file, $description)
+    {
+        $fileInfo = $file->getFileInfo();
+
+        if (isset($fileInfo['pic']) && $fileInfo['pic']['error'] == 0) {
+            $sub = substr($fileInfo['pic']['name'], -4);
+            $filename = time() . $sub;
+            $file->addFilter('Rename', array('target' => APPLICATION_PATH . '/../public/upload/' . $filename, 'overwrite' => true));
+
+            $file->receive();
+
+            $this->model->addSliderShow(
+                $filename,
+                $description
+            );
+        }
+
+        $this->model->save();
+    }
+
+    /**
+     * @param int $index
+     */
+    public function delSliderShow($index)
+    {
+        $this->model->delSliderShow($index);
+        $this->model->save();
+    }
+
+    /**
+     * @return array
+     */
+    public function getSliderShows()
+    {
+        return $this->model->getSliderShows();
     }
 }
