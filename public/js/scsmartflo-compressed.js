ce41c3cbef701c17100881658710079c7aa8d40e
eric <eric7578@gmail.com>
2015-10-01 23:17:39 +0800

    f2e #compile

--- public/js/scsmartflo-compressed.js
+++ public/js/scsmartflo-compressed.js
@@ -1,3 +1,109 @@
+/*
+ * Placeholder plugin for jQuery
+ * ---
+ * Copyright 2010, Daniel Stocks (http://webcloud.se)
+ * Released under the MIT, BSD, and GPL Licenses.
+ */
+(function($) {
+    function Placeholder(input) {
+        this.input = input;
+        if (input.attr('type') == 'password') {
+            this.handlePassword();
+        }
+        // Prevent placeholder values from submitting
+        $(input[0].form).submit(function() {
+            if (input.hasClass('placeholder') && input[0].value == input.attr('placeholder')) {
+                input[0].value = '';
+            }
+        });
+    }
+    Placeholder.prototype = {
+        show: function(loading) {
+            // FF and IE saves values when you refresh the page. If the user refreshes the page with
+            // the placeholders showing they will be the default values and the input fields won't be empty.
+            if (this.input[0].value === '' || (loading && this.valueIsPlaceholder())) {
+                if (this.isPassword) {
+                    try {
+                        this.input[0].setAttribute('type', 'text');
+                    } catch (e) {
+                        this.input.before(this.fakePassword.show()).hide();
+                    }
+                }
+                this.input.addClass('placeholder');
+                this.input[0].value = this.input.attr('placeholder');
+            }
+        },
+        hide: function() {
+            if (this.valueIsPlaceholder() && this.input.hasClass('placeholder')) {
+                this.input.removeClass('placeholder');
+                this.input[0].value = '';
+                if (this.isPassword) {
+                    try {
+                        this.input[0].setAttribute('type', 'password');
+                    } catch (e) {}
+                    // Restore focus for Opera and IE
+                    this.input.show();
+                    this.input[0].focus();
+                }
+            }
+        },
+        valueIsPlaceholder: function() {
+            return this.input[0].value == this.input.attr('placeholder');
+        },
+        handlePassword: function() {
+            var input = this.input;
+            input.attr('realType', 'password');
+            this.isPassword = true;
+            // IE < 9 doesn't allow changing the type of password inputs
+            if ($.browser.msie && input[0].outerHTML) {
+                var fakeHTML = $(input[0].outerHTML.replace(/type=(['"])?password\1/gi, 'type=$1text$1'));
+                this.fakePassword = fakeHTML.val(input.attr('placeholder')).addClass('placeholder').focus(function() {
+                    input.trigger('focus');
+                    $(this).hide();
+                });
+                $(input[0].form).submit(function() {
+                    fakeHTML.remove();
+                    input.show()
+                });
+            }
+        }
+    };
+    var NATIVE_SUPPORT = !! ("placeholder" in document.createElement("input"));
+    $.fn.placeholder = function() {
+        return NATIVE_SUPPORT ? this : this.each(function() {
+            var input = $(this);
+            var placeholder = new Placeholder(input);
+            placeholder.show(true);
+            input.focus(function() {
+                placeholder.hide();
+            });
+            input.blur(function() {
+                placeholder.show(false);
+            });
+
+            // On page refresh, IE doesn't re-populate user input
+            // until the window.onload event is fired.
+            if ($.browser.msie) {
+                $(window).load(function() {
+                    if (input.val()) {
+                        input.removeClass("placeholder");
+                    }
+                    placeholder.show(true);
+                });
+                // What's even worse, the text cursor disappears
+                // when tabbing between text inputs, here's a fix
+                input.focus(function() {
+                    if (this.value == "") {
+                        var range = this.createTextRange();
+                        range.collapse(true);
+                        range.moveStart('character', 0);
+                        range.select();
+                    }
+                });
+            }
+        });
+    }
+})(jQuery);
 /**
  * Created by eric7578 on 15/8/30.
  */
@@ -11,6 +117,10 @@ $(function () {
     var $productKindList = $('.products-kinds li');
     //聯絡我們
     var $contactUs = $('.contact-us');
+
+    //placeholder
+    $contactUs.find('.form-control').placeholder();
+
     //導覽列
     var $navbarItem = $('.navbar-item');
 
@@ -64,7 +174,7 @@ $(function () {
 
                 //img
                 $('<img/>')
-                    .attr('src', '/img/' + productData.id + '.jpg')
+                    .attr('src', '/upload/' + productData.pic)
                     .appendTo($product);
 
                 //name
@@ -119,6 +229,7 @@ $(function () {
 
     function showProductDetail ($showTarget) {
         var $productDescribe = $productDetail.find('.product-describe');
+        $productDetail.find('.product-image img').attr('src', $showTarget.find('img').attr('src'));
         $productDescribe.find('h3').text($showTarget.find('.product-name').text());
         $productDescribe.find('p').text($showTarget.find('.product-describe').text());
         $productDetail.toggle(0, function () {
