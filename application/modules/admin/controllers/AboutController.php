0ff520384f5d2b917ddf861ee726819030c47320
MilesChou <jangconan@gmail.com>
2015-10-01 22:11:38 +0800

    Updated about admin controller

--- application/modules/admin/controllers/AboutController.php
+++ application/modules/admin/controllers/AboutController.php
@@ -14,6 +14,7 @@ class Admin_AboutController extends Zend_Controller_Action
 
         $this->view->description = $aboutService->getDescription();
         $this->view->title = $aboutService->getTitle();
+        $this->view->pic = $aboutService->getPic();
     }
 
     public function saveAction()
@@ -23,10 +24,12 @@ class Admin_AboutController extends Zend_Controller_Action
 
         $description = $this->getRequest()->getParam('description');
         $title = $this->getRequest()->getParam('title');
+        $file = new Zend_File_Transfer_Adapter_Http();
 
         $aboutService = new Application_Service_About();
         $aboutService->setDescription($description);
         $aboutService->setTitle($title);
+        $aboutService->setPic($file);
         $aboutService->save();
 
         $this->redirect('/admin/about');
