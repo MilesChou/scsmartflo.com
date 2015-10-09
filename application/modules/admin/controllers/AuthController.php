e737a33f5d89a8c7f3835c094432003baa3357f9
MilesChou <jangconan@gmail.com>
2015-10-10 01:47:55 +0800

    Added auth controller/view

---
+++ application/modules/admin/controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+class Admin_AuthController extends Zend_Controller_Action
+{
+    public function init()
+    {
+    }
+
+    public function indexAction()
+    {
+        $this->getHelper('layout')->disableLayout();
+    }
+
+    public function loginAction()
+    {
+        $this->getHelper('viewRenderer')->setNoRender();
+        $this->getHelper('layout')->disableLayout();
+
+        $password = $this->getRequest()->getParam('password');
+
+        $authService = new Application_Service_Auth();
+        $authService->login($password);
+
+        $this->redirect('/admin');
+    }
+}
