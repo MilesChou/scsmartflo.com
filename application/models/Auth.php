c8de2540eb01fb7ff681b65424cc5aad8b210493
MilesChou <jangconan@gmail.com>
2015-10-10 01:36:09 +0800

    Added auth model

---
+++ application/models/Auth.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Contact model
+ *
+ * @author Miles
+ */
+class Application_Model_Auth
+{
+    const CONFIG_KEY = 'auth';
+
+    /**
+     * @var Application_Model_DbTable_Row_Config
+     */
+    private $configRow;
+
+    /**
+     * @var array
+     */
+    private $data;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $configTable = new Application_Model_DbTable_Config();
+        $this->configRow = $configTable->getConfigRow();
+        $this->data = $this->configRow->getConfig(self::CONFIG_KEY);
+
+        if ($this->data === null) {
+            $this->data = array(
+                'password' => '0000',
+            );
+
+            $this->save();
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function getInfo()
+    {
+        return $this->data;
+    }
+
+    /**
+     * 儲存
+     */
+    public function save()
+    {
+        $this->configRow->setConfig(self::CONFIG_KEY, $this->data);
+        $this->configRow->save();
+    }
+
+    /**
+     * @param array $data
+     */
+    public function setInfo($data)
+    {
+        $this->data = $data;
+    }
+}
