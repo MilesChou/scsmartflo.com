eec6a6739f7b054617d5d592691746c2feffb956
MilesChou <jangconan@gmail.com>
2015-10-10 01:41:57 +0800

    Added auth service

---
+++ application/services/Auth.php
@@ -0,0 +1,52 @@
+<?php
+
+class Application_Service_Auth
+{
+    /**
+     * @var Application_Model_About
+     */
+    private $model;
+
+    /**
+     * @var Zend_Session_Namespace
+     */
+    private $session;
+
+    /**
+     * @var array
+     */
+    private $data;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->session = new Zend_Session_Namespace('default');
+        $this->model = new Application_Model_Auth();
+        $this->data = $this->model->getInfo();
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isLogin()
+    {
+        return isset($this->session->login);
+    }
+
+    /**
+     * @param string $password
+     */
+    public function login($password)
+    {
+        if ($this->data['password'] == $password) {
+            $this->session->login = true;
+        }
+    }
+
+    public function logout()
+    {
+        unset($this->session->login);
+    }
+}
