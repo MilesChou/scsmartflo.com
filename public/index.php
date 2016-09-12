baa8f8a2c9c43c95d6e9662de0caa7d111798487
MilesChou <jangconan@gmail.com>
2016-09-12 16:36:57 +0800

    Fix entrypoint

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+
+// Create application, bootstrap, and run
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPLICATION_PATH . '/configs/application.ini'
+);
+
+$application->bootstrap()->run();
